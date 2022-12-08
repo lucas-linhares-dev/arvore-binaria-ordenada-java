49557e935742f20c1442eab60e51a978659522a6
Lucas <lucas_linhares_@hotmail.com>
2022-12-08 12:11:09 -0300

    Teste de inserção implementado - Minha árvore x Árvore do java. Trabalho finalizado.

--- src/Teste.java
+++ src/Teste.java
@@ -1,22 +1,49 @@
+import java.util.Iterator;
+import java.util.TreeSet;
 import java.util.Random;
 
 public class Teste {
-
+	
+	public static String imprimeLista(TreeSet arvore){
+	    Iterator<Integer> i = arvore.iterator();
+	    while (i.hasNext()){
+	        System.out.println(i.next() + " ");
+	    }
+	    return("");
+	}
+	
+	
 	public static void main(String[] args) {
-		ArvoreBinariaOrdenada<Integer> arvore = new ArvoreBinariaOrdenada<Integer>();
+		TreeSet<Integer> arvoreJava = new TreeSet<>();
+		ArvoreBinariaOrdenada<Integer> minhaArvore = new ArvoreBinariaOrdenada<>();
 		Random gerador = new Random();
 		
-		long tempoInicial = System.currentTimeMillis();
+		long tInicialArvoreJava = System.currentTimeMillis();
 		
 		for(int i = 0 ; i < 1000000 ; i++) {
-			arvore.adicionar(gerador.nextInt());
+			arvoreJava.add(gerador.nextInt());
 		}
-	    
-	    long tempoFinal = System.currentTimeMillis();
-	    
-	    System.out.println("Tempo de execução: "+(tempoFinal-tempoInicial));
-	    
-
+		
+		long tFinalArvoreJava = System.currentTimeMillis();
+		
+		long tInicialMinhaArvore = System.currentTimeMillis();
+		
+		for(int i = 0 ; i < 1000000 ; i++) {
+			minhaArvore.adicionar(gerador.nextInt());
+		}
+		
+		long tFinalMinhaArvore = System.currentTimeMillis();
+		
+		
+		System.out.println("Inserção de UM MILHÂO de elementos.");
+		
+		System.out.println("Tempo Treeset do java: "+(tFinalArvoreJava - tInicialArvoreJava));
+		
+		System.out.println("Tempo minha árvore: "+(tFinalMinhaArvore - tInicialMinhaArvore));
+		
+		System.out.println();
+		
+		System.out.println("Minha árvore sempre ganha :)");
 	}
 
 }
